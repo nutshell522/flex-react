c5746746aa177edc64c6b6d9c30fd50983ef1f58
TedYin <jjboy830227@gmail.com>
2024-06-03 08:43:07 +0800

    commit

--- FlexCore/FlexCore/Mappings/ProductProfile.cs
+++ FlexCore/FlexCore/Mappings/ProductProfile.cs
@@ -8,11 +8,25 @@ namespace FlexCore.Mappings
 {
     public class ProductProfile :Profile
     {
-        public ProductProfile()
+		public ProductProfile()
 		{
-			CreateMap<ProductEntity, ProductDto>();
-			CreateMap<ProductDto, ProductEntity>();
+			// Mapping for BottomCategoryEntity to BottomCategoryDto
+			CreateMap<BottomCategoryEntity, BottomCategoryDto>();
 
+			// Mapping for ProductColorEntity to ProductColorDto
+			CreateMap<ProductColorEntity, ProductColorDto>();
+
+			// Mapping for ProductEntity to ProductDto
+			CreateMap<ProductEntity, ProductDto>()
+				.ForMember(dest => dest.BottomCategory, opt => opt.MapFrom(src => src.BottomCategory != null ? src.BottomCategory : null))
+				.ForMember(dest => dest.ProductColors, opt => opt.MapFrom(src => src.ProductColors != null ? src.ProductColors : new List<ProductColorEntity>()));
+
+			// Reverse mapping for ProductDto to ProductEntity
+			CreateMap<ProductDto, ProductEntity>()
+				.ForMember(dest => dest.BottomCategory, opt => opt.MapFrom(src => src.BottomCategory != null ? src.BottomCategory : null))
+				.ForMember(dest => dest.ProductColors, opt => opt.MapFrom(src => src.ProductColors != null ? src.ProductColors : new List<ProductColorDto>()));
+
+			// Mapping for ProductDto to ProductListVM
 			CreateMap<ProductDto, ProductListVM>()
 				.ForMember(dest => dest.ProductColors, opt => opt.MapFrom(src => MapProductColors(src.ProductColors)));
 		}
